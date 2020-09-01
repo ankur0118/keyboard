900cb8c61694d78f75b58f02670c7c43628b83cc
ankur0118 <58909679+ankur0118@users.noreply.github.com>
2020-09-01 09:24:08 +0530

    Update keyboard.go

--- keyboard.go
+++ keyboard.go
@@ -10,15 +10,15 @@ import (
 )
 
 func GetFloat() (float64, error) {
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
+	reader := bufio.NewReader(os.Stdin) //the buffer input output method reads reads the I/P
+	input, err := reader.ReadString('\n') //the entered data is read till newline char and saved in the variable input and the error if saved in err
 	if err != nil {
 		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)
-	number, err := strconv.ParseFloat(input, 64)
+	}//checks if there is an error in the data read
+	input = strings.TrimSpace(input) //removes all white spaces
+	number, err := strconv.ParseFloat(input, 64) // converts the input into the float 64  
 	if err != nil {
 		log.Fatal(err)
-	}
-	return number, nil
+	} // checks if there is an error in the input
+	return number, nil //returns the converted input and nil for error 
 }
